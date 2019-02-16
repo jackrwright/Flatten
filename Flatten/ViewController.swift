ca3e6918507d951a4891a098b73cf71fe4c0a8de
Jack Wright <jackrwright1@gmail.com>
2019-02-16 10:23:05 -0500

    Initial Commit

---
+++ Flatten/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flatten
+//
+//  Created by Jack Wright on 2/16/19.
+//  Copyright © 2019 Jack Wright. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
